25c90ac8b429f57a8d59e2bd51e072f8729b4fc6
Vihar Shah <84446743+vihar2712@users.noreply.github.com>
2025-06-08 22:10:47 +0000

    removed another migration

--- db/migrate/20250418004442_change_to_polymorphic_association_in_teams.rb
+++
@@ -1,7 +0,0 @@
-class ChangeToPolymorphicAssociationInTeams < ActiveRecord::Migration[8.0]
-  def change
-    # Remove old assignment and course references
-    remove_reference :teams, :assignment, foreign_key: true
-    remove_reference :teams, :course, foreign_key: true
-  end
-end
